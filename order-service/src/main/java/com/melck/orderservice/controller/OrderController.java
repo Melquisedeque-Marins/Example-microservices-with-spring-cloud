b6a0f4df9af9c4b12697c34526131dd2f959a7cc
Melquisedeque <melck_junior@hotmail.com>
2022-12-19 18:51:12 +0000

    Application.yml adjusts

--- order-service/src/main/java/com/melck/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/melck/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.melck.orderservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class OrderController {
     @Autowired
     private OrderService service;
     
+    @PostMapping
     public Order bookOrder(@RequestBody Order order) {
         return service.saveOrder(order);
     }
--- payment-service/src/main/java/com/melck/paymentservice/controller/PaymentController.java
+++ payment-service/src/main/java/com/melck/paymentservice/controller/PaymentController.java
@@ -1,6 +1,7 @@
 package com.melck.paymentservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class PaymentController {
     @Autowired
     private PaymentService service;
 
-
+    @PostMapping
     public Payment doPayment(@RequestBody Payment payment) {
         return service.doPayment(payment);
     }
