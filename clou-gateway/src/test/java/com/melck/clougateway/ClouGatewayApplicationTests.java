ee9189e027ca4332af2c8bd4d61359d02ab2bb2c
Melquisedeque <melck_junior@hotmail.com>
2022-12-24 17:33:17 +0000

    Gateway

---
+++ clou-gateway/src/test/java/com/melck/clougateway/ClouGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melck.clougateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClouGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
